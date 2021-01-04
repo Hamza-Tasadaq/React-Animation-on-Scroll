17d8b850b95eb599d3a45e417c6ea6906d85d016
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2021-01-04 09:22:06 +0500

    Scroll Animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react'
 import './App.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000
+    });
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Animation On Scroll</h1>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <div data-aos="zoom-in-down" className="container">
+        1
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div data-aos="zoom-in-up" className="container">
+        2
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div data-aos="fade-right" className="container">
+        3
+      </div> <br />
+      <br />
+      <br />
+      <br />
+      <div data-aos="fade-left" className="container">
+        4
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div data-aos="flip-right" className="container">
+        5
+      </div> <br />
+      <br />
+      <br />
+      <br />
+      <div data-aos="flip-left" className="container">
+        6
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div data-aos="zoom-in-up" className="container">
+        7
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+
     </div>
   );
 }
